450e5e9ce05b9c1cc6c59fd1d574d29767ac32a8
Sepler Gábor <sepler.gabor@gmail.com>
2017-03-23 23:25:24 +0100

    search_palindrome function working for 3 characters

--- Week-02/weekly-project/palindrome_sg.py
+++ Week-02/weekly-project/palindrome_sg.py
@@ -1,3 +1,5 @@
+#Create a function named create palindrome following your current language's style guide. It should take a string, create a palindrome from it and then return it.
+
 def create_palindrome(valami):
     palindrome = str(valami)
     for i in range(len(valami), 0, -1):
