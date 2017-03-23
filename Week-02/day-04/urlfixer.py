fa72d87e5e7e14a2da4091f48882af7e2499e1cf
Sepler Gábor <sepler.gabor@gmail.com>
2017-03-23 15:12:54 +0100

    url fixed

---
+++ Week-02/day-04/urlfixer.py
@@ -0,0 +1,9 @@
+# Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+# Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+url = "https//www.reddit.com/r/nevertellmethebots"
+
+print(url)
+url_fixed = url[:-4] + "odds"
+print(url_fixed)
+print(url_fixed + "/")
