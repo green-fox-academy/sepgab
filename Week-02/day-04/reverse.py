aa56a0cc409033633480d2dbfadb3a35c8847c14
Sepler Gábor <sepler.gabor@gmail.com>
2017-03-23 13:24:31 +0100

    Two strings and one list exercise solved

---
+++ Week-02/day-04/reverse.py
@@ -0,0 +1,5 @@
+# Reverse the string
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+print(reversed[::-1])
---
+++ Week-02/day-04/solarsystem.py
@@ -0,0 +1,8 @@
+# Saturn is missing from the planetList
+# Insert it into the correct position
+
+planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
+
+planetList.insert(5, "Saturn")
+
+print(planetList)
