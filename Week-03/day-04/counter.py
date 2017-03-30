547513f3d6155e8c6b9ee5ac4be3756b3969ab82
Sepler Gábor <sepler.gabor@gmail.com>
2017-03-30 10:13:10 +0200

    Its the final countdown

---
+++ Week-03/day-04/counter.py
@@ -0,0 +1,11 @@
+
+# Write a recursive function that takes one parameter: n and counts down from n.
+
+def counter(num):
+    if num == 0:
+        print(0)
+    if num != 0:
+        print(num)
+        return counter(num-1)
+
+counter(10)
