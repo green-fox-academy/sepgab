1b39791946ad84d4846ff8266f2085a9f2409232
Sepler Gábor <sepler.gabor@gmail.com>
2017-03-30 15:16:56 +0200

    Exercise 11 solved

---
+++ Week-03/day-04/exercise11.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import random
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+width_screen = 300
+
+canvas.create_rectangle(0, 0, width_screen, width_screen, fill='yellow')
+
+
+def drawlines(width, num, origo_x, origo_y):
+    if num == 0:
+        canvas.create_line(origo_x-width/6, origo_y-width/2, origo_x-width/6, origo_y+width/2)
+        canvas.create_line(origo_x+width/6, origo_y-width/2, origo_x+width/6, origo_y+width/2)
+        canvas.create_line(origo_x-width/2, origo_y-width/6, origo_x+width/2, origo_y-width/6)
+        canvas.create_line(origo_x-width/2, origo_y+width/6, origo_x+width/2, origo_y+width/6)
+    else:
+        canvas.create_line(origo_x-width/6, origo_y-width/2, origo_x-width/6, origo_y+width/2)
+        canvas.create_line(origo_x+width/6, origo_y-width/2, origo_x+width/6, origo_y+width/2)
+        canvas.create_line(origo_x-width/2, origo_y-width/6, origo_x+width/2, origo_y-width/6)
+        canvas.create_line(origo_x-width/2, origo_y+width/6, origo_x+width/2, origo_y+width/6)
+        drawlines(width/3, num-1, origo_x, origo_y-width/3)
+        drawlines(width/3, num-1, origo_x, origo_y+width/3)
+        drawlines(width/3, num-1, origo_x-width/3, origo_y)
+        drawlines(width/3, num-1, origo_x+width/3, origo_y)
+
+
+
+
+drawlines(300, 3, 150, 150)
+
+
+
+
+
+
+root.mainloop()
