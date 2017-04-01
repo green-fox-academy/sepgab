db6b0ecdddb43a7acfe66a266a5a284bd3d7c173
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-01 08:12:23 +0200

    Lottery exercise solved.

--- Week-03/day-02/lottery.py
+++ Week-03/day-02/lottery.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 # Create a method that returns the five most frequent lottery number in a pretty table format
 my_file = open('otos.csv', "r")
 
@@ -6,7 +8,7 @@ my_file = open('otos.csv', "r")
 
 #list_games = my_file.readlines()
 str_games = ''.join(my_file.readlines())
-list_games = str_games.split('\n')
+list_games = str_games.split('"\n')
 sep_list_games = []
 list_number = []
 numbers = []
@@ -17,11 +19,21 @@ for i in list_games:
 for i in range(len(sep_list_games)):
     list_number.append(sep_list_games[i][11:16])
 
-for i in range(len(list_number)):
+for i in range(0, len(list_number)-1):
     for j in range(0, 5):
         numbers.append(list_number[i][j])
 
-print(numbers)
+dict_numbers = {}
+
+for i in numbers:
+    if i not in dict_numbers:
+        dict_numbers[i] =1
+    else:
+        dict_numbers[i] += 1
+
+sorted_list = sorted(dict_numbers.items(), key=itemgetter(1))
+
+print(sorted_list[-5:])
 
 #print(sep_list_games[0][11:16])
 #print(sep_list_games[1][11:16])
