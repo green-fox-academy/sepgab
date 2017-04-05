de278d8c39c3712792fa64b9da551015b00ddebb
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-05 13:59:06 +0200

    feat. anagrams, test anagrams and letter counter in two files

--- Week-04/day-03/sepgab_test.py
+++ Week-04/day-03/sepgab_test.py
@@ -1,11 +1,51 @@
 import unittest
-from sepgab_work import Apples
+from sepgab_work import Apples, Operations, anagram_checker
 
-class TestCounter(unittest.TestCase):
+class TestApples(unittest.TestCase):
 
     def test_apple(self):
         object1 = Apples()
         self.assertEqual(object1.get_apple(), 'Apple')
+
+class TestOperations(unittest.TestCase):
+
+    def test_sum_method(self):
+        list = [1, 2, 3, 5]
+        object1 = Operations()
+        self.assertEqual(object1.sum(list), 11)
+
+    def test_sum_with_empty_list(self):
+        list = []
+        object1 = Operations()
+        self.assertEqual(object1.sum(list), 0)
+
+    def test_sum_with_one_element(self):
+        list = [5]
+        object1 = Operations()
+        self.assertEqual(object1.sum(list), 5)
+
+    def test_sum_with_more_elements(self):
+        list = [5, 15, 485, 2]
+        object1 = Operations()
+        self.assertEqual(object1.sum(list), 507)
+
+    def test_sum_with_null(self):
+        list = [0]
+        object1 = Operations()
+        self.assertEqual(object1.sum(list), 0)
+
+class TestAnagrams(unittest.TestCase):
+
+    def test_anagrams_function_one_letter(self):
+        self.assertEqual(anagram_checker('a', 'b'), False)
+
+    def test_anagrams_function_more_letters(self):
+        self.assertEqual(anagram_checker('abba', 'baba'), True)
+
+    def test_anagrams_function_more_letters_tricky(self):
+        self.assertEqual(anagram_checker('abba ', 'babab'), False)
+
+
 
 
 
--- Week-04/day-03/sepgab_work.py
+++ Week-04/day-03/sepgab_work.py
@@ -2,3 +2,28 @@ class Apples():
 
     def get_apple(self):
         return 'Apple'
+
+class Operations():
+
+    def sum(self, list_of_int):
+        self.total = 0
+        for num in list_of_int:
+            self.total += num
+        return self.total
+
+def anagram_checker(string1, string2):
+    list_string1 = list(string1)
+    list_string1.sort()
+    list_string2 = list(string2)
+    list_string2.sort()
+    return  list_string1 == list_string2
+
+def letter_counter(string):
+    dict_letters = {}
+    string_list = list(string)
+    for letter in string_list:
+        if letter not in dict_letters:
+            dict_letters[letter] = 1
+        else:
+            dict_numbers[letter] += 1
+            
