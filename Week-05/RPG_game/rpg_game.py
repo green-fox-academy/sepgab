b1a21445b010a205cdba5c0976e2038fc2ba6371
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-11 13:52:37 +0200

    Floors and walls drawn for level 1

--- Week-05/RPG_game/rpg_game.py
+++ Week-05/RPG_game/rpg_game.py
@@ -10,15 +10,36 @@ class Area():
         self.floor_tile = PhotoImage(file="floor.png")
         self.floor_tile.zoom(6, 6)
         self.floor_tile.subsample(5, 5)
+        self.wall_tile = PhotoImage(file="wall.png")
+        self.wall_tile.zoom(6, 6)
+        self.wall_tile.subsample(5, 5)
 
-    def draw_tile(self, x, y):
+    def draw_floor(self, x, y):
         canvas.create_image(x,y, anchor=NW, image=self.floor_tile)
 
+    def draw_wall(self, x, y):
+        canvas.create_image(x,y, anchor=NW, image=self.wall_tile)
+
+map_level_1 = [
+[1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+[1, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+[1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+[1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+[1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+[1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+[1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+[1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+[1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+[1, 0, 1, 0, 1, 0, 1, 1, 1, 1]]
 
 area = Area()
-for x in range(10):
-    for y in range(11):
-        area.draw_tile(60*x, 60*y)
+for y in range(len(map_level_1)):
+    for x in range(len(map_level_1[y])):
+        if map_level_1[y][x] == 1:
+            area.draw_floor(60*x, 60*y)
+        else:
+            area.draw_wall(60*x, 60*y)
 
 
 
