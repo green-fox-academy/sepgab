74ba4261dc9502ebe8e17221ebacf8c5104e5f87
Sepler Gábor <sepler.gabor@gmail.com>
2017-04-12 18:21:49 +0200

    Almost there

--- Week-05/rpg_game/test.py
+++ Week-05/rpg_game/test.py
@@ -1,9 +1,9 @@
 import random
 
-print(random.randint(0, 10))
-print(random.randint(0, 10))
-print(random.randint(0, 10))
-print(random.randint(0, 10))
-print(random.randint(0, 10))
-print(random.randint(0, 10))
-print(random.randint(0, 10))
+a = False
+number = 3
+
+if random.randint(1, number) == 1:
+    a = True
+
+print(a)
