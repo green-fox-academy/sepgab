edd6ac9899fefd53cdf8870f6a475c3a1f70d0fe
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-02 13:25:47 +0200

    starting JavaScript...

---
+++ Week-07/Day-02/hellome.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// Modify this program to greet you instead of the World!
+var name = 'Gabor'
+console.log('Hello, ' + name);
---
+++ Week-07/Day-02/test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('Alma');
+
+var a = 23;
+
+if (a === 123) {
+  console.log('YEEEY');
+} else if a === 23) {
+  var b = 11;
+  console.log('Not so YEEEY');
+} else {
+  console.log('kacsa');
+}
+
+console.log(b);
+
+var i = 0
+while(i < 10) {
+  i++;
+  console.log(i);
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
+function greet() {
+  console.log('Majom')
+}
+
+greet();
