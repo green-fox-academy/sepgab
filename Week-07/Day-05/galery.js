ffb2e4e35e686322124a4fde7c6c27a519fda25e
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-05 11:15:53 +0200

    galery filed up

--- Week-07/Day-05/galery.js
+++ Week-07/Day-05/galery.js
@@ -1,40 +1,23 @@
 'use strict';
 
-var imageData = [
-  {
-    source: 'images\image1.jpg',
-    title: 'Image1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    source: 'images\image2.jpg',
-    title: 'Image2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    source: 'images\image3.jpg',
-    title: 'Image3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    source: 'images\image4.jpg',
-    title: 'Image4',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    source: 'images\image5.jpg',
-    title: 'Image5',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  }
-]
-
-
+var mainImage = document.querySelector('.main_image');
 var thumbnails = document.querySelector('.thumbnails');
 
-for (var i = 0; i < imageData.length; i++) {
-  var imgToAdd = document.createElement('img');
-  imgToAdd.style.width = '150px';
-  imgToAdd.style.height = '100px';
-  imgToAdd.setAttribute('src', imageData[i].source);
-  thumbnails.appendChild(imgToAdd);
+function thumbnailFiller() {
+  for (var i = 0; i < imageData.length; i++) {
+    var imgToAdd = document.createElement('img');
+    imgToAdd.style.maxWidth = '150px';
+    imgToAdd.style.height = '100px';
+    imgToAdd.style.margin = '5px';
+    imgToAdd.setAttribute('src', imageData[i].source);
+    thumbnails.appendChild(imgToAdd);
+  }
 }
+
+thumbnailFiller();
+
+function imageFiller(index=0) {
+  mainImage.setAttribute('src', imageData[index].source);
+}
+
+imageFiller();
---
+++ Week-07/Day-05/images.js
@@ -0,0 +1,27 @@
+var imageData = [
+  {
+    source: 'images\\image1.jpg',
+    title: 'Image1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    source: 'images\\image2.jpg',
+    title: 'Image2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    source: 'images\\image3.jpg',
+    title: 'Image3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    source: 'images\\image4.jpg',
+    title: 'Image4',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    source: 'images\\image5.jpg',
+    title: 'Image5',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  }
+]
