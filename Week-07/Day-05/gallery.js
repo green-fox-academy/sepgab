4930e7e84f251f1b4ea0bb27b4a1726a12274e2b
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-05 18:01:01 +0200

    gallery so far...

--- Week-07/Day-05/gallery.js
+++ Week-07/Day-05/gallery.js
@@ -1,21 +1,18 @@
 'use strict';
 
 var mainImage = document.querySelector('.main_image');
+var title = document.querySelector('.title')
+var descr = document.querySelector('.descr')
 var thumbnails = document.querySelector('.thumbnails');
 var leftButton = document.querySelector('.leftarrow');
 var rightButton = document.querySelector('.rightarrow');
 var index = 0;
 
 function thumbnailFiller() {
-  for (var i = 0; i < imageData.length; i++) {
+  for (let i = 0; i < imageData.length; i++) {
     var imgToAdd = document.createElement('img');
-    imgToAdd.style.maxWidth = '150px';
-    imgToAdd.style.height = '100px';
-    imgToAdd.style.margin = '5px';
-    imgToAdd.style.border = '4px solid white';
-    imgToAdd.style.borderRadius = '5px';
-    imgToAdd.style.boxShadow = '0 20px 30px grey';
     imgToAdd.setAttribute('src', imageData[i].source);
+    imgToAdd.addEventListener('click', function(){mainImage.setAttribute('src', imageData[i].source); title.innerText = imageData[i].title; descr.textContent = imageData[i].description; index = i; return index});
     thumbnails.appendChild(imgToAdd);
   }
 }
@@ -24,26 +21,43 @@ thumbnailFiller();
 
 function imageFiller(num=0) {
   mainImage.setAttribute('src', imageData[num].source);
+  title.innerText = imageData[index].title;
+  descr.textContent = imageData[index].description;
+  borderer();
 }
 
 imageFiller(index);
 
+function borderer() {
+  var imageList = document.querySelectorAll('.thumbnails img')
+  console.log( imageList );
+  imageList.forEach(function(el) {
+    el.style.borderColor = 'white';
+  })
+
+  console.log( index+1 );
+  var childToBlack = document.querySelector('.thumbnails img:nth-child(' + index+1 + ')')
+  childToBlack.style.borderColor = 'black';
+}
+
 
 function imageRight() {
-  index += 1;
+  index++;
   if (index === imageData.length) {
     index = 0;
   }
   imageFiller(index);
+  borderer();
   return index;
 }
 
 function imageLeft() {
-  index -= 1;
+  index--;
   if (index === -1) {
     index = imageData.length-1;
   }
   imageFiller(index);
+  borderer();
   return index;
 }
 
--- Week-07/Day-05/images.js
+++ Week-07/Day-05/images.js
@@ -1,27 +1,27 @@
 var imageData = [
   {
     source: 'images\\image1.jpg',
-    title: 'Image1',
+    title: 'Kata & flowers',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     source: 'images\\image2.jpg',
-    title: 'Image2',
+    title: 'Dani & umbrella',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     source: 'images\\image3.jpg',
-    title: 'Image3',
+    title: 'Beach boy Dani',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     source: 'images\\image4.jpg',
-    title: 'Image4',
+    title: 'Misi at home',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   },
   {
     source: 'images\\image5.jpg',
-    title: 'Image5',
+    title: 'Kitchen life',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   }
 ]
