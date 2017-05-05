a60ac7a1f64b2079fa50a68b1709a490dc6e6c0f
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-05 13:42:49 +0200

    galery sliding and css enhanced

--- Week-07/Day-05/galery.js
+++ Week-07/Day-05/galery.js
@@ -2,6 +2,9 @@
 
 var mainImage = document.querySelector('.main_image');
 var thumbnails = document.querySelector('.thumbnails');
+var leftButton = document.querySelector('.leftarrow');
+var rightButton = document.querySelector('.rightarrow');
+var index = 0;
 
 function thumbnailFiller() {
   for (var i = 0; i < imageData.length; i++) {
@@ -9,6 +12,9 @@ function thumbnailFiller() {
     imgToAdd.style.maxWidth = '150px';
     imgToAdd.style.height = '100px';
     imgToAdd.style.margin = '5px';
+    imgToAdd.style.border = '4px solid white';
+    imgToAdd.style.borderRadius = '5px';
+    imgToAdd.style.boxShadow = '0 20px 30px grey';
     imgToAdd.setAttribute('src', imageData[i].source);
     thumbnails.appendChild(imgToAdd);
   }
@@ -16,8 +22,30 @@ function thumbnailFiller() {
 
 thumbnailFiller();
 
-function imageFiller(index=0) {
-  mainImage.setAttribute('src', imageData[index].source);
+function imageFiller(num=0) {
+  mainImage.setAttribute('src', imageData[num].source);
 }
 
-imageFiller();
+imageFiller(index);
+
+
+function imageRight() {
+  index += 1;
+  if (index === imageData.length) {
+    index = 0;
+  }
+  imageFiller(index);
+  return index;
+}
+
+function imageLeft() {
+  index -= 1;
+  if (index === -1) {
+    index = imageData.length-1;
+  }
+  imageFiller(index);
+  return index;
+}
+
+leftButton.addEventListener('click', imageLeft);
+rightButton.addEventListener('click', imageRight);
