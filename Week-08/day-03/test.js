96ad2ef16aa4f87124658ad4a2278c3b41611e39
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-10 10:29:12 +0200

    call.js solved

--- Week-08/day-03/test.js
+++ Week-08/day-03/test.js
@@ -11,3 +11,45 @@ var bigger = numbers.filter(function(num){
 console.log( doubleNumbers );
 
 console.log( bigger );
+
+
+// asynchronity
+
+function greet1() {
+  console.log('hi!');
+}
+
+function greet2() {
+  console.log('hello!');
+}
+
+function greet3() {
+  console.log('waazup!');
+}
+
+greet1();
+setTimeout(greet2, 1000);
+greet3();
+
+
+function greet4() {
+  console.log('hi!');
+}
+
+function greet5(anything) {
+  anything();
+}
+
+function greet6() {
+  console.log('HEHE!');
+}
+
+greet5(greet4);
+
+
+function substract(a, b) {
+  return a - b;
+}
+
+substract.pear = 45;
+console.log(substract.pear);
