41c26a4d6a152222c67eaf8d4ca3997e0cc63119
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-22 17:42:00 +0200

    feat(TennisGame2): game 2 enhanced

--- Week-10/day-01/TennisGame2.js
+++ Week-10/day-01/TennisGame2.js
@@ -1,64 +1,46 @@
 var TennisGame2 = function() {
     this.P1point = 0;
     this.P2point = 0;
-};
-
-TennisGame2.prototype.getScore = function() {
-    var score = "";
-    var scoreArray = ["Love", "Fifteen", "Thirty", "Forty"]
-
-    if (this.P1point === this.P2point && this.P1point < 3) {
-      score = scoreArray[this.P1point] + '-All';
-    } else if (this.P1point === this.P2point && this.P1point > 2) {
-      score = "Deuce";
-    } else {
-      score = scoreArray[this.P1point] + "-" + scoreArray[this.P2point];
-    }
-
-    if (this.P1point > this.P2point && this.P2point >= 3) {
-        score = "Advantage player1";
-    }
-
-    if (this.P2point > this.P1point && this.P1point >= 3) {
-        score = "Advantage player2";
-    }
-
-    if (this.P1point >= 4 && this.P2point >= 0 && (this.P1point - this.P2point) >= 2) {
-        score = "Win for player1";
-    }
-    if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
-        score = "Win for player2";
-    }
-    return score;
-};
-
-TennisGame2.prototype.SetP1Score = function(number) {
-    var i;
-    for (i = 0; i < number; i++) {
-        this.P1Score();
-    }
-};
-
-TennisGame2.prototype.SetP2Score = function(number) {
-    var i;
-    for (i = 0; i < number; i++) {
-        this.P2Score();
-    }
-};
-
-TennisGame2.prototype.P1Score = function() {
-    this.P1point++;
-};
-
-TennisGame2.prototype.P2Score = function() {
-    this.P2point++;
+    this.standing = "";
 };
 
 TennisGame2.prototype.wonPoint = function(player) {
-    if (player === "player1")
-        this.P1Score();
-    else
-        this.P2Score();
+  if (player === "player1") {
+    this.P1point++;
+  } else {
+    this.P2point++;
+  }
 };
+
+TennisGame2.prototype.getScore = function() {
+  var scoreArray = ["Love", "Fifteen", "Thirty", "Forty"]
+  if (this.P1point === this.P2point && this.P1point < 3) {
+    this.standing = scoreArray[this.P1point] + '-All';
+  } else if (this.P1point === this.P2point && this.P1point > 2) {
+    this.standing = "Deuce";
+  } else {
+    this.standing = scoreArray[this.P1point] + "-" + scoreArray[this.P2point];
+    if (this.P1point > 2 && this.P2point > 2) {
+      this.scoreAnalyzer(this.P1point, this.P2point);
+    }
+  }
+  return this.standing;
+};
+
+TennisGame1.prototype.scoreAnalyzer = function (score1, score2) {
+  let gamer = '';
+  if (score1 > score2) {
+    gamer = '1'
+  } else {
+    gamer = '2';
+  }
+  if (Math.abs(score1 - score2) === 1) {
+    this.standing = "Advantage player" + gamer;
+  } else {
+    this.standing = "Win for player" + gamer;
+  }
+  return this.standing;
+}
+
 
 module.exports = TennisGame2;
