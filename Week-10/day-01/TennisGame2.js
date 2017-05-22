ef8e5ffacccba6511dfc04126105b6943b0fc7c9
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-22 17:16:42 +0200

    feat(TennisGame2): some bullsh*t deleted

--- Week-10/day-01/TennisGame2.js
+++ Week-10/day-01/TennisGame2.js
@@ -1,8 +1,6 @@
 var TennisGame2 = function() {
     this.P1point = 0;
     this.P2point = 0;
-    this.P1res = "";
-    this.P2res = "";
 };
 
 TennisGame2.prototype.getScore = function() {
@@ -11,54 +9,10 @@ TennisGame2.prototype.getScore = function() {
 
     if (this.P1point === this.P2point && this.P1point < 3) {
       score = scoreArray[this.P1point] + '-All';
-    }
-    if (this.P1point === this.P2point && this.P1point > 2)
-        score = "Deuce";
-
-    if (this.P1point > 0 && this.P2point === 0) {
-        if (this.P1point === 1)
-            this.P1res = "Fifteen";
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        if (this.P1point === 3)
-            this.P1res = "Forty";
-
-        this.P2res = "Love";
-        score = this.P1res + "-" + this.P2res;
-    }
-    if (this.P2point > 0 && this.P1point === 0) {
-        if (this.P2point === 1)
-            this.P2res = "Fifteen";
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        if (this.P2point === 3)
-            this.P2res = "Forty";
-
-        this.P1res = "Love";
-        score = this.P1res + "-" + this.P2res;
-    }
-
-    if (this.P1point > this.P2point && this.P1point < 4) {
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        if (this.P1point === 3)
-            this.P1res = "Forty";
-        if (this.P2point === 1)
-            this.P2res = "Fifteen";
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        score = this.P1res + "-" + this.P2res;
-    }
-    if (this.P2point > this.P1point && this.P2point < 4) {
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        if (this.P2point === 3)
-            this.P2res = "Forty";
-        if (this.P1point === 1)
-            this.P1res = "Fifteen";
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        score = this.P1res + "-" + this.P2res;
+    } else if (this.P1point === this.P2point && this.P1point > 2) {
+      score = "Deuce";
+    } else {
+      score = scoreArray[this.P1point] + "-" + scoreArray[this.P2point];
     }
 
     if (this.P1point > this.P2point && this.P2point >= 3) {
