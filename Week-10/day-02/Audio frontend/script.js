4ef1fdef3a0b9a2f112de307fd9f12c30cd4829e
Sepler Gábor <sepler.gabor@gmail.com>
2017-05-23 18:25:11 +0200

    Audio frontend started

---
+++ Week-10/day-02/Audio frontend/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const addPlaylist = document.querySelector('addPlaylist');
+const rewind = document.querySelector('rewind');
+const pause = document.querySelector('pause');
+const forward = document.querySelector('forward');
+const shuffle = document.querySelector('shuffle');
+const addTrack = document.querySelector('addTrack');
+const addToFav = document.querySelector('addToFav');
+
+
+console.log('hello world');
+
+
+
+addPlaylist.addEventListener('click', addPlaylist);
+rewind.addEventListener('click', rewind);
+pause.addEventListener('click', pause);
+forward.addEventListener('click', forward);
+shuffle.addEventListener('click', shuffle);
+addTrack.addEventListener('click', addTrack);
+addToFav.addEventListener('click', addToFav);
